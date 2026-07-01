b613ac239bbb55d67e6b540362b2b474c81f52d2
sam <sam@DESKTOP-VUC0HGA>
2026-07-01 23:50:33 +0100

    created the user class nothing too special

---
+++ bartering/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace bartering.Models
+{
+    public class User
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        DateOnly DateOfBirth { get; set; }
+         
+    }
+}
