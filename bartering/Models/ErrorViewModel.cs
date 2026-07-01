753e55f3564a91f233a81c079ff4cec05bf20e3d
sam <sam@DESKTOP-VUC0HGA>
2026-07-01 01:22:50 +0100

    Add project files.

---
+++ bartering/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bartering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
